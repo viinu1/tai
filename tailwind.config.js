4b34b744bdf57cab79459d5f3e45f70e7a0b4518
Van Vi <pvanvi2508@gmail.com>
2024-03-17 18:21:14 +0700

    tintuc

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./public/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                card: "0 10px 40px 0 rgb(39 37 63 / 15%),0 2px 9px 0 rgb(39 37 63 / 15%)",
+            },
+        },
     },
     plugins: [],
 };
